db94fdf9248b7aaabf0d0c97d312a190ee4e77bd
badrabbit96 <badrabbit96@gmail.com>
2018-10-25 11:34:02 +0200

    + Adress MAC

--- Main/src/main/MainWindow.java
+++ Main/src/main/MainWindow.java
@@ -1,8 +1,10 @@
-
 package main;
 
-
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -20,6 +22,17 @@ public class MainWindow extends javax.swing.JFrame {
         setLocationRelativeTo(null);
 
         getIPAddres();
+        loadData();
+
+    }
+
+    public void loadData() {
+
+        try {
+            yourIP.setText(InetAddress.getLocalHost().getHostAddress());
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(MainWindow.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 
@@ -48,6 +61,7 @@ public class MainWindow extends javax.swing.JFrame {
                         if (address.isReachable(5000)) {
                             String hostname = address.getHostName();
                             String ip = address.getHostAddress();
+                            byte[] tmac = address.getAddress();
 
                             System.out.println("Adres IP: " + output);
 
@@ -58,28 +72,53 @@ public class MainWindow extends javax.swing.JFrame {
                             System.out.println("Ping: " + ping.substring(0, 1) + "ms");
 
                             if (hostname.equals(output)) {
-                                System.out.println("Brak nazwy hosta");
+                                //System.out.println("Brak nazwy hosta");
+                                hostname = "n/a";
                             } else {
-                                System.out.println("Nazwa hosta:" + hostname);
+                                //System.out.println("Nazwa hosta:" + hostname);
+
+                            }
+
+//                            System.out.println(tmac);
+//
+//                            StringBuilder sb = new StringBuilder();
+//                            for (int i = 0; i < tmac.length; i++) {
+//                                sb.append(String.format("%02X%s", tmac[i], (i < tmac.length - 1) ? "-" : ""));
+//                            }
+//                            System.out.println(sb.toString());
+                            String macAdress = "n/a";
+                            if (output.equals(InetAddress.getLocalHost().getHostAddress())) {
+                               InetAddress ip1 = InetAddress.getByName(output);
+
+                            NetworkInterface network = NetworkInterface.getByInetAddress(ip1);
+
+                            byte[] mac = network.getHardwareAddress();
+
+                            StringBuilder sb = new StringBuilder();
+                            for (int i = 0; i < mac.length; i++) {
+                                sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+                            }
+                            System.out.println(sb.toString());
+                            macAdress = sb.toString();
+                                
                             }
 
                             DefaultTableModel model = (DefaultTableModel) jTable1.getModel();
-                            model.addRow(new Object[]{output, hostname, ping.substring(0, 1)});
+                            model.addRow(new Object[]{output, hostname, ping.substring(0, 1),macAdress});
 
-//                        InetAddress ip1 = InetAddress.getByName(output);
+//                            InetAddress ip1 = InetAddress.getByName(output);
 //
-//                       NetworkInterface network = NetworkInterface.getByInetAddress(ip1);
-//                       
-//                       byte[] mac = network.getHardwareAddress();
-//                      
-//      //        System.out.print("Current MAC address : ");
-//                      
-//              StringBuilder sb = new StringBuilder();
-//              for (int i = 0; i < mac.length; i++) {
-//                      sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));            
-//              }
-//              System.out.println(sb.toString());
-// 
+//                            NetworkInterface network = NetworkInterface.getByInetAddress(ip1);
+//
+//                            byte[] mac = network.getHardwareAddress();
+//
+//                            //        System.out.print("Current MAC address : ");
+//                            StringBuilder sb = new StringBuilder();
+//                            for (int i = 0; i < mac.length; i++) {
+//                                sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+//                            }
+//                          //  System.out.println(sb.toString());
+//                           
                         } else {
                             //  System.out.println("Not Reachable: "+output);
                         }
@@ -105,6 +144,7 @@ public class MainWindow extends javax.swing.JFrame {
         jTable1 = new javax.swing.JTable();
         jButton1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
+        yourIP = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -113,10 +153,13 @@ public class MainWindow extends javax.swing.JFrame {
 
             },
             new String [] {
-                "Adres IP", "Nazwa hosta", "Ping"
+                "Adres IP", "Nazwa hosta", "Ping", "Adres MAC"
             }
         ));
         jScrollPane1.setViewportView(jTable1);
+        if (jTable1.getColumnModel().getColumnCount() > 0) {
+            jTable1.getColumnModel().getColumn(0).setPreferredWidth(20);
+        }
 
         jButton1.setText("Skanuj");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -127,6 +170,9 @@ public class MainWindow extends javax.swing.JFrame {
 
         jLabel1.setText("Twój adres IP:");
 
+        yourIP.setForeground(new java.awt.Color(0, 0, 255));
+        yourIP.setText("adresIP");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -134,14 +180,14 @@ public class MainWindow extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 733, Short.MAX_VALUE)
-                        .addContainerGap())
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 733, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jLabel1)
-                        .addGap(154, 154, 154))))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(yourIP, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -149,7 +195,8 @@ public class MainWindow extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1))
+                    .addComponent(jLabel1)
+                    .addComponent(yourIP))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(31, 31, 31))
@@ -203,5 +250,6 @@ public class MainWindow extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
+    private javax.swing.JLabel yourIP;
     // End of variables declaration//GEN-END:variables
 }
